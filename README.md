ca5b23947a702818dd58ec949f6cd564074d0b14
kesha1225 <samedov03@mail.ru>
2022-02-17 14:34:22 +0300

    new method

--- README.md
+++ README.md
@@ -50,3 +50,36 @@ async def main():
 
 asyncio.get_event_loop().run_until_complete(main())
 ```
+
+
+### Парсинг популярных групп, лайк, коммент, фолоу
+```python3
+import asyncio
+
+from tonplace import get_token, API
+
+
+async def main():
+    token = await get_token("+79123456789")
+    api = API(token)
+
+    popular_groups = await api.search(tab="groups", sort="popular")
+    for group_id in popular_groups["groups"]:
+        group_data = await api.get_group(group_id)
+        posts = group_data["posts"]
+        target_post = posts[0]
+
+        target_id = target_post["id"]
+        await api.write_comment(target_id, text="test")
+        await api.like(target_id)
+        print(
+            f"Написали коммент и лайкнули пост - https://ton.place/group{group_id}?w=post{target_id}"
+        )
+
+        post_author = target_post["ownerId"]
+        await api.follow(post_author)
+        print(f"Зафоловились на автора поста - https://ton.place/id{post_author}")
+
+
+asyncio.get_event_loop().run_until_complete(main())
+```
--- tonplace/api.py
+++ tonplace/api.py
@@ -21,6 +21,7 @@ class API:
             "Sec-GPC": "1",
         }
 
+    # TODO: фолоу лайки
     async def request(
         self,
         method: str,
@@ -29,15 +30,35 @@ class API:
         json_data: Optional[dict] = None,
     ):
         resp = await self.session.request(
-            method, self.base_path + path, data=data, json=json_data
+            method,
+            self.base_path + path,
+            data=data,
+            json=json_data,
+            headers=self.headers,
         )
+        if resp.status == 500:
+            raise ValueError("Site is down")
         json_response = json.loads(await resp.text())
         if json_response.get("code") == "fatal":
             raise ValueError(f"Ошибка запроса - {json_response.get('message')}")
         return json_response
 
     async def get_user(self, user_id: int):
+        """
+        Информация о юзере в том числе посты
+        :param user_id:
+        :return:
+        """
         user = await self.request("POST", path=f"profile/{user_id}")
+        return user
+
+    async def get_group(self, group_id: int):
+        """
+        Информация о группе в том числе посты
+        :param group_id:
+        :return:
+        """
+        user = await self.request("POST", path=f"/group/{group_id}")
         return user
 
     async def search(
@@ -61,6 +82,43 @@ class API:
                 "tab": tab,
                 "sort": sort,
                 "city": city,
+            },
+        )
+        return result
+
+    async def follow(self, user_id: int):
+        result = await self.request(
+            "POST",
+            path=f"follow/{user_id}/add",
+        )
+        return result
+
+    async def like(self, post_id: int):
+        result = await self.request(
+            "POST",
+            path=f"likes/{post_id}/post/add",
+        )
+        return result
+
+    async def write_comment(
+        self,
+        post_id: int,
+        text: str,
+        attachments: Optional[list] = None,
+        reply: Optional[int] = 0,
+        group_id: Optional[int] = 0,
+    ):
+        if attachments is None:
+            attachments = []
+        result = await self.request(
+            "POST",
+            path=f"posts/new",
+            json_data={
+                "parentId": post_id,
+                "replyTo": reply,
+                "text": text,
+                "attachments": attachments,
+                "groupId": group_id,
             },
         )
         return result
